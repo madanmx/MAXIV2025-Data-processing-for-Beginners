bcacb5253d54798e1d4a8d8f9c1a132b01a3ccdc
Madan Kumar Shankar <madan.mx@gmail.com>
2025-08-03 12:10:42 +0200

    Update stats.sh

--- stats.sh
+++ stats.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-PREFIX=$1
+HKLFILE=$1
 CELLFILE=$2
 RESO=$3
 
 #!/bin/bash
 
-check_hkl ${PREFIX}.hkl -y 6/mmm -p ${CELLFILE} --highres=${RESO} --nshells=20 --shell-file=check.dat
+check_hkl ${HKLFILE}.hkl -y 6/mmm -p ${HKLFILE} --highres=${RESO} --nshells=20 --shell-file=check.dat
 
-compare_hkl ${PREFIX}.hkl1 ${PREFIX}.hkl2 -y 6/mmm -p ${CELLFILE} --highres=${RESO} --fom=rsplit --nshells=20 --shell-file=rsplit.dat
+compare_hkl ${HKLFILE}.hkl1 ${HKLFILE}.hkl2 -y 6/mmm -p ${HKLFILE} --highres=${RESO} --fom=rsplit --nshells=20 --shell-file=rsplit.dat
 
-compare_hkl ${PREFIX}.hkl1 ${PREFIX}.hkl2 -y 6/mmm -p ${CELLFILE} --highres=${RESO} --fom=ccstar --nshells=20 --shell-file=ccstar.dat
+compare_hkl ${HKLFILE}.hkl1 ${HKLFILE}.hkl2 -y 6/mmm -p ${HKLFILE} --highres=${RESO} --fom=ccstar --nshells=20 --shell-file=ccstar.dat
 
-compare_hkl ${PREFIX}.hkl1 ${PREFIX}.hkl2 -y 6/mmm -p ${CELLFILE} --highres=${RESO} --fom=cc --nshells=20 --shell-file=cc.dat
+compare_hkl ${HKLFILE}.hkl1 ${HKLFILE}.hkl2 -y 6/mmm -p ${HKLFILE} --highres=${RESO} --fom=cc --nshells=20 --shell-file=cc.dat
 
 cat rsplit.dat ccstar.dat cc.dat check.dat > statistics.dat
 
