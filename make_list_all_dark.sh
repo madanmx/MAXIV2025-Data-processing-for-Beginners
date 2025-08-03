3f160a4f1aafdb6629011b8191bbacfdada00ea0
Madan Kumar Shankar <madan.mx@gmail.com>
2025-08-03 11:57:15 +0200

    Update make_list_all_dark.sh

--- make_list_all_dark.sh
+++ make_list_all_dark.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#Assume you have to use all .h5 files and make a dataset.lst file  
-ls /gpfs/offline1/visitors/micromax/path-to-dataset/*.h5 > dark.lst
+#Assume you have to use all .h5 files of a particular run and want to make a dataset.lst file  
+ls /gpfs/offline1/visitors/micromax/path-to-run/*.h5 > dataset.lst
 
 
