74bfc5b2eddd90f4eaf6274f038932360737c154
Madan Kumar Shankar <madan.mx@gmail.com>
2025-08-03 12:15:02 +0200

    Update stats.sh

--- stats.sh
+++ stats.sh
@@ -1,21 +1,25 @@
 #!/bin/bash
 
+# Usage: ./script.sh HKLFILE CELLFILE RESOLUTION SYMMETRY
+
 HKLFILE=$1
 CELLFILE=$2
 RESO=$3
+SYMMETRY=$4
 
-#!/bin/bash
+# Check for required arguments
+if [ $# -ne 4 ]; then
+  echo "Usage: $0 <HKLFILE_BASE> <CELLFILE> <RESOLUTION> <SPACE_GROUP>"
+  exit 1
+fi
 
-check_hkl ${HKLFILE}.hkl -y 6/mmm -p ${HKLFILE} --highres=${RESO} --nshells=20 --shell-file=check.dat
+# Run checks
+check_hkl "${HKLFILE}.hkl" -y "${SYMMETRY}" -p "${HKLFILE}" --highres="${RESO}" --nshells=20 --shell-file=check.dat
 
-compare_hkl ${HKLFILE}.hkl1 ${HKLFILE}.hkl2 -y 6/mmm -p ${HKLFILE} --highres=${RESO} --fom=rsplit --nshells=20 --shell-file=rsplit.dat
+# Run comparisons with different figures of merit
+compare_hkl "${HKLFILE}.hkl1" "${HKLFILE}.hkl2" -y "${SYMMETRY}" -p "${HKLFILE}" --highres="${RESO}" --fom=rsplit --nshells=20 --shell-file=rsplit.dat
+compare_hkl "${HKLFILE}.hkl1" "${HKLFILE}.hkl2" -y "${SYMMETRY}" -p "${HKLFILE}" --highres="${RESO}" --fom=ccstar --nshells=20 --shell-file=ccstar.dat
+compare_hkl "${HKLFILE}.hkl1" "${HKLFILE}.hkl2" -y "${SYMMETRY}" -p "${HKLFILE}" --highres="${RESO}" --fom=cc --nshells=20 --shell-file=cc.dat
 
-compare_hkl ${HKLFILE}.hkl1 ${HKLFILE}.hkl2 -y 6/mmm -p ${HKLFILE} --highres=${RESO} --fom=ccstar --nshells=20 --shell-file=ccstar.dat
-
-compare_hkl ${HKLFILE}.hkl1 ${HKLFILE}.hkl2 -y 6/mmm -p ${HKLFILE} --highres=${RESO} --fom=cc --nshells=20 --shell-file=cc.dat
-
+# Combine all statistics into a single file
 cat rsplit.dat ccstar.dat cc.dat check.dat > statistics.dat
-
-EOF
-
-
